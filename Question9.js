2fe32a419328f95f795f41f404152bc0405e21ec
kushal_fs <34955452+kushal-Fs@users.noreply.github.com>
2020-10-27 16:15:05 +0530

    Create Question9.js

---
+++ Question9.js
@@ -0,0 +1,8 @@
+var Output = (function(x)
+        {
+        Delete X;
+        return X;
+        }
+        )(0);
+        console.log(output);
+        //Uncaught SyntaxError: Unexpected identifier
